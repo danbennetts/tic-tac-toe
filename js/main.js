cae2c619f43d96df94c5b4377d3cc22ef69d81ea
Daniel Bennetts <daniel.bennetts@bigpond.com>
2016-03-08 21:19:22 +1100

    Add some functionality, click event

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 $(document).ready(function(){
 
-console.log("hello!");
+//What are the main things we need to keep track of and do?
+
+//There are two players and we need to find out who's currently playing (whether it is X or 0)
+
+  //We need to store both players information so that it is accessible.
+
+var playerOne = {
+  icon: 'X',
+  name: 'Player One',
+};
+
+var playerTwo = {
+  icon: 'O',
+  name: 'Player Two',
+};
+
+// Check if the event listener works when you click on a column
+$('.game-board').on('click', '.column', function(event) {
+  //Create a variable that can store the current event handler.
+  //Have prefixed it with $ so I can identify it later.
+  var $column = $(event.currentTarget);
+  $column.addClass('column-x');
+});
 
 
 
